af97e93f8018fcd65cb7d67be5c37c12c68acfd0
Mauricio Massayuki <99891691+MauricioMSoares@users.noreply.github.com>
2023-09-03 01:35:50 -0300

    feat: navegação da tela inicial com telas de cadastro e login implementada

--- app/src/main/java/com/petsup/MainActivity.kt
+++ app/src/main/java/com/petsup/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.petsup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun navigateToSignup(view: View) {
+        val signupButton: Button = findViewById(R.id.main_screen_signup_button)
+        signupButton.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun navigateToLogin(view: View) {
+        val loginButton: Button = findViewById(R.id.main_screen_login_button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
