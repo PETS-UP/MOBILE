77fb747a9f3194e8b24c056e8c6729dffc8e43d0
MauricioMSoares <mauricio.soares@sptech.school>
2023-09-04 22:25:33 -0300

    feat: navbar implementada 4/5. tela de listagem de pets unida com versão vazia, falta lógica

---
+++ app/src/main/java/com/petsup/PetListActivity.kt
@@ -0,0 +1,11 @@
+package com.petsup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PetListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_list)
+    }
+}
