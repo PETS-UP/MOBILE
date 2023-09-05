385795001ef9bd115cdd96c5fa68af8851cae6ec
belthenrique <henrique.beltrao@sptech.school>
2023-09-04 22:24:22 -0300

    Funções incompletas para validação de email e senha

--- app/src/main/java/com/petsup/ValidationObject.kt
+++ app/src/main/java/com/petsup/ValidationObject.kt
@@ -1,7 +1,6 @@
 package com.petsup
 
 import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
 
 object ValidationObject {
     fun nameValidation(etName: EditText): Boolean {
@@ -13,6 +12,51 @@ object ValidationObject {
             false
         } else {
             true
+        }
+    }
+
+    fun emailValidation(etEmail: EditText): Boolean {
+        val str: String = ".com"
+        return if (etEmail.text.toString().isEmpty()) {
+            etEmail.error = "Preencha o campo."
+            false
+        } else if (!etEmail.text.toString().contains('@') ||
+            !etEmail.text.toString().contains(str)
+        ) {
+            etEmail.error = "Insira um e-mail válido!"
+            false
+        } else {
+            true
+        }
+    }
+
+    fun passwordValidation(etPassword: EditText): Boolean {
+        val str: String = ".com"
+        return if (etPassword.text.toString().isEmpty()) {
+            false
+        } else if (etPassword.text.toString().length >= 8 &&
+            etPassword.text.toString().length <= 15){
+            var ch: Char
+            var capitalFlag = false
+            var lowerCaseFlag = false
+            var numberFlag = false
+            for (i in 0 until str.length) {
+                ch = str[i]
+                if (Character.isDigit(ch)) {
+                    numberFlag = true
+                } else if (Character.isUpperCase(ch)) {
+                    capitalFlag = true
+                } else if (Character.isLowerCase(ch)) {
+                    lowerCaseFlag = true
+                }
+                if (numberFlag && capitalFlag && lowerCaseFlag) return true
+            }
+            return false
+            etPassword.error = "Insira um e-mail válido!"
+            false
+        } else {
+            etPassword.error = "A senha deve possuir entre 8 e 15 caracteres"
+            false
         }
     }
 }
