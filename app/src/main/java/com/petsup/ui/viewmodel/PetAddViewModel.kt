9971160188a526ef9ce72e047d9993e1879f3c3c
Mauricio Massayuki <99891691+MauricioMSoares@users.noreply.github.com>
2023-11-20 21:03:11 -0300

    feat: adicionado escopo de coroutines nos métodos de cadastro de pet

--- app/src/main/java/com/petsup/ui/viewmodel/PetAddViewModel.kt
+++ app/src/main/java/com/petsup/ui/viewmodel/PetAddViewModel.kt
@@ -3,9 +3,12 @@ package com.petsup.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.petsup.api.Rest
 import com.petsup.models.pet.PetResposta
 import com.petsup.services.PetService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class PetAddViewModel: ViewModel() {
     private var _petStack = MutableLiveData<List<PetResposta>>()
@@ -15,17 +18,17 @@ class PetAddViewModel: ViewModel() {
         Rest.getInstance().create(PetService::class.java)
     }
 
-    fun postPetStack(obj: String) {
+    fun postPetStack(obj: String) = viewModelScope.launch(Dispatchers.IO) {
         val request = api.addToStack(obj)
         request.execute()
     }
 
-    fun popFromStack() {
+    fun popFromStack() = viewModelScope.launch(Dispatchers.IO) {
         val request = api.popFromStack()
         request.execute()
     }
 
-    fun postPet() {
+    fun postPet() = viewModelScope.launch(Dispatchers.IO) {
         val request = api.postPet()
         request.execute()
     }
