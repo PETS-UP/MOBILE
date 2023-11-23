5c4dc116ebf5b156458617a0c77ed79d8d750241
Mauricio Massayuki <99891691+MauricioMSoares@users.noreply.github.com>
2023-11-23 01:36:36 -0300

    feat: adicionados endpoints para conectar com filtros da API

--- app/src/main/java/com/petsup/services/PetshopService.kt
+++ app/src/main/java/com/petsup/services/PetshopService.kt
@@ -2,11 +2,14 @@ package com.petsup.services
 
 import com.petsup.models.petshop.Petshop
 import com.petsup.models.petshop.PetshopAvaliacao
+import com.petsup.models.petshop.PetshopMediaAvaliacao
+import com.petsup.models.petshop.PetshopMediaPreco
 import retrofit2.Call
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PetshopService {
@@ -22,7 +25,15 @@ interface PetshopService {
     @POST("petshops/inscrever/{idPetshop}")
     fun subscribeToPetshop(@Query("idPetshop") idPetshop: Int): Call<Void>
 
+    @GET("clientes/ordenar-media-preco")
+    fun getPetshopsOrderByPrecoAsc(): Call<List<PetshopMediaPreco>>
 
-    @GET("petshops/media-avaliacao")
-    fun getMediaAvaliacao(): Call<List<PetshopAvaliacao>>
+    @GET("clientes/ordenar-media-avaliacao")
+    fun getPetshopsOrderByMedia(): Call<List<PetshopMediaAvaliacao>>
+
+    @GET("clientes/petshops-proximos/{idCliente}")
+    fun getPetshopsByClienteBairro(@Path("idCliente") idCliente: Int): Call<List<Petshop>>
+
+    @GET("favoritos/{idCliente}")
+    fun getFavoritos(@Path("idCliente") idCliente: Int): Call<List<Petshop>>
 }
