d05cf4c774513c04b1ed92abbc5f819bd47cd755
belthenrique <henrique.beltrao@sptech.school>
2023-11-23 22:26:32 -0300

    reversão do método de busca de usuário em ProfileDataViewModel

--- app/src/main/java/com/petsup/ui/viewmodel/ProfileDataViewModel.kt
+++ app/src/main/java/com/petsup/ui/viewmodel/ProfileDataViewModel.kt
@@ -8,7 +8,9 @@ import androidx.lifecycle.viewModelScope
 import com.petsup.api.Rest
 import com.petsup.models.cliente.ClienteDetalhes
 import com.petsup.models.servico.ServicoResposta
+import com.petsup.services.AgendamentoService
 import com.petsup.services.ClienteService
+import com.petsup.services.ServicoService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -17,51 +19,55 @@ import retrofit2.Callback
 import retrofit2.HttpException
 import retrofit2.Response
 import retrofit2.await
+import retrofit2.create
 
 class ProfileDataViewModel : ViewModel() {
-    private var _profileData = MutableLiveData<List<ServicoResposta>>()
-    val profileData: LiveData<List<ServicoResposta>> = _profileData
-    lateinit var clienteDetalhes: ClienteDetalhes
+    private var _profileData = MutableLiveData<ClienteDetalhes>()
+    val profileData: LiveData<ClienteDetalhes> = _profileData
+//    lateinit var clienteDetalhes: ClienteDetalhes
 
     private val api by lazy {
         Rest.getInstance().create(ClienteService::class.java)
     }
 
-    fun getUserById(idCliente: Int): ClienteDetalhes {
-        var request: Response<ClienteDetalhes>
-//        api.getClienteById(idCliente)
-//                 .enqueue(object : Callback<ClienteDetalhes> {
-//                        override fun onResponse(
-//                            call: Call<ClienteDetalhes>,
-//                            response: Response<ClienteDetalhes>
-//                        ) {
-//                            if (response.isSuccessful) {
-//                                clienteDetalhes = response.body()!!
-//                            }
-//                        }
-//                        override fun onFailure(call: Call<ClienteDetalhes>, t: Throwable) {
-//                            Log.d("Request Error", "Erro na requisição de ID $t")
-//                        }
+    fun getUserById(idCliente: Int){
+//        var request: Response<ClienteDetalhes>
+//        viewModelScope.launch {
+//            request = requestGetUser(idCliente)
+//            if (request.isSuccessful){
 //
-//                    })
-        viewModelScope.launch {
-            request = requestGetUser(idCliente)
-            if (request.isSuccessful){
+//            }
+//        }
+//        return request.body()!!
 
-            }
+        viewModelScope.launch(Dispatchers.IO) {
+            api.getClienteById(idCliente)
+                .enqueue(object : Callback<ClienteDetalhes> {
+                    override fun onResponse(
+                        call: Call<ClienteDetalhes>,
+                        response: Response<ClienteDetalhes>
+                    ) {
+                        if (response.isSuccessful) {
+                            _profileData.value = response.body()!!
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ClienteDetalhes>, t: Throwable) {
+                        Log.d("Request Error", "Erro na requisição de busca de cliente $t")
+                    }
+                })
         }
-        return request.body()!!
     }
 
-    private suspend fun requestGetUser(idCliente: Int): Response<ClienteDetalhes> {
-        return withContext(Dispatchers.IO) {
-            try {
-                val request = api.getClienteById(idCliente)
-                val response = request.await()
-                Response.success(response)
-            } catch (e: HttpException) {
-                Response.error(e.code(), e.response()?.errorBody())
-            }
-        }
-    }
+//    private suspend fun requestGetUser(idCliente: Int): Response<ClienteDetalhes> {
+//        return withContext(Dispatchers.IO) {
+//            try {
+//                val request = api.getClienteById(idCliente)
+//                val response = request.await()
+//                Response.success(response)
+//            } catch (e: HttpException) {
+//                Response.error(e.code(), e.response()?.errorBody())
+//            }
+//        }
+//    }
 }
